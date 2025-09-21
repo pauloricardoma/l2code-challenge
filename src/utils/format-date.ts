841c953825410d98b4b8e9ee200003a58cb46516
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2025-09-21 08:03:47 -0300

    Feat: date format utilitary function

---
+++ src/utils/format-date.ts
@@ -0,0 +1,13 @@
+export const formatDate = (date: string) => {
+  return date.split('-').reverse().join('/');
+}
+
+export const formatLocalDateWithTime = (date: Date) => {
+  return date.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
